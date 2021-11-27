caaadac93f3797972eb03140a72d5bda3173507b
adrielrobles <adropere@espol.edu.ec>
2021-11-27 14:19:10 -0500

    Creacion de modulos y shared

---
+++ src/app/modules/catalogos/catalogos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CatalogosRoutingModule } from './catalogos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CatalogosRoutingModule
+  ]
+})
+export class CatalogosModule { }
---
+++ src/app/modules/nosotros/nosotros.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NosotrosRoutingModule } from './nosotros-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NosotrosRoutingModule
+  ]
+})
+export class NosotrosModule { }
