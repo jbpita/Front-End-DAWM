65f1385858f20f7eb9726c6837a0f978fe391d27
adrielrobles <adropere@espol.edu.ec>
2022-01-10 02:13:11 -0500

    componente carrito con funcionalidad

--- src/app/modules/carrito/carrito-routing.module.ts
+++ src/app/modules/carrito/carrito-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'',
+  component:CarritoComponent
+}
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/modules/carrito/carrito.module.ts
+++ src/app/modules/carrito/carrito.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarritoRoutingModule } from './carrito-routing.module';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarritoComponent
+  ],
   imports: [
     CommonModule,
     CarritoRoutingModule
